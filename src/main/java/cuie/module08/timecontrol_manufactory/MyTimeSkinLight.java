815e82de1937860689755a9903731d791dccf665
Mario <mario.wettstein@students.fhnw.ch>
2020-04-15 12:40:21 +0200

    Add_Color_Basic_Desing

--- src/main/java/cuie/module08/timecontrol_manufactory/MyTimeSkinLight.java
+++ src/main/java/cuie/module08/timecontrol_manufactory/MyTimeSkinLight.java
@@ -1,14 +1,17 @@
 
 package cuie.module08.timecontrol_manufactory;
 
+        import javafx.geometry.Pos;
         import javafx.scene.control.Label;
         import javafx.scene.control.SkinBase;
         import javafx.scene.control.TextField;
         import javafx.scene.image.Image;
         import javafx.scene.image.ImageView;
+        import javafx.scene.layout.HBox;
         import javafx.scene.layout.VBox;
         import javafx.scene.shape.Circle;
         import javafx.scene.shape.Ellipse;
+        import javafx.scene.text.TextAlignment;
         import javafx.util.converter.LocalTimeStringConverter;
 
 
@@ -31,6 +34,7 @@ class MyTimeSkinLight extends SkinBase<MyTimeControl> {
     private TextField editableTime;
     private Label captionLabel;
     private Ellipse ellipseLight;
+    private VBox oClockFrame;
 
     MyTimeSkinLight(MyTimeControl control) {
         super(control);
@@ -56,13 +60,21 @@ class MyTimeSkinLight extends SkinBase<MyTimeControl> {
         captionLabel = new Label();
         captionLabel.getStyleClass().add("caption-label");
 
+
         ellipseLight = new Ellipse(center, center, 50,50);
         ellipseLight.getStyleClass().add("ellipse-light");
+
+        oClockFrame = new VBox(editableTime, captionLabel);
+        oClockFrame.setAlignment(Pos.CENTER);
     }
 
     private void layoutParts() {
+
+
         //getChildren().addAll(placeHolder);
-        getChildren().addAll(ellipseLight, captionLabel, editableTime);
+        // getChildren().addAll(ellipseLight,editableTime, captionLabel);
+        getChildren().addAll(ellipseLight,oClockFrame);
+
     }
 
     private void setupValueChangeListeners() {
